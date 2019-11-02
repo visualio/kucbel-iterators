6d93a63829d3a792fb540784037334685436e78e
David Kucbel <david.kucbel@email.cz>
2019-11-02 14:35:25 +0100

    Void iterator

---
+++ src/VoidIterator.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Kucbel\Iterators;
+
+use Countable;
+use Iterator;
+use Nette\SmartObject;
+
+class VoidIterator implements Countable, Iterator
+{
+	use SmartObject;
+
+	/**
+	 * @return void
+	 */
+	function rewind() : void
+	{
+
+	}
+
+	/**
+	 * @return void
+	 */
+	function next() : void
+	{
+
+	}
+
+	/**
+	 * @return bool
+	 */
+	function valid() : bool
+	{
+		return false;
+	}
+
+	/**
+	 * @return null
+	 */
+	function current()
+	{
+		return null;
+	}
+
+	/**
+	 * @return null
+	 */
+	function key()
+	{
+		return null;
+	}
+
+	/**
+	 * @return int
+	 */
+	function count() : int
+	{
+		return 0;
+	}
+
+	/**
+	 * @return array
+	 */
+	function toArray() : array
+	{
+		return [];
+	}
+}
